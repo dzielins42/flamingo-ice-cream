e3f2ca448c8a678c9ac184b4140914e2c828f650
dzielins42 <dzielins42@gmail.com>
2020-02-18 20:44:16 +0100

    Create Category model

--- django/ranshigen/models.py
+++ django/ranshigen/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, default=None)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    # TODO image
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
