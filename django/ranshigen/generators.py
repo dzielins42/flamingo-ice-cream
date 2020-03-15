36909ab8c343017a5ba7c6ce3752664ddc36019e
dzielins42 <dzielins42@gmail.com>
2020-03-15 19:09:13 +0100

    Create Generator model and logic to parse its data

---
+++ django/ranshigen/generators.py
@@ -0,0 +1,108 @@
+import numpy
+import xml.etree.ElementTree
+
+class Generator:
+    def generate(self, count):
+        return None
+
+    def _getValue(self, value):
+        if isinstance(value, Generator):
+            return value.generate(1)[0]
+        else:
+            return str(value)
+
+class RandomGenerator(Generator):
+    v = None
+    p = None
+
+    def __init__(self, values = None, probabilities = None):
+        Generator.__init__(self)
+        self.v = values
+        self.p = probabilities
+
+    def generate(self, count):
+        result = list(numpy.random.choice(self.v, p=self.p, size=count))
+        return result
+
+class JoinGenerator(Generator):
+    separator = None
+    values = None
+
+    def __init__(self, values = None, separator = ""):
+        Generator.__init__(self)
+        self.values = values
+        self.separator = separator
+
+    def generate(self, count):
+        result = []
+        for i in range(count):
+            result.append(self.separator.join(self._getValue(value) for value in self.values))
+        return result
+
+class XmlParser:
+    data = {}
+
+    def fromFile(self, path):
+        tree = xml.etree.ElementTree.parse(path)
+        return self._parseGenerator(tree.getroot())
+
+    def fromString(self, stringData):
+        root = xml.etree.ElementTree.fromstring(stringData)
+        return self._parseGenerator(root)
+
+    def  _parseGenerator(self, element):
+        generator = None
+        if element.tag == "random":
+            generator = self._parseRandomGenerator(element)
+        elif element.tag == "space-join":
+                generator = self._parseSpaceJoinGenerator(element)
+        elif element.tag == "join":
+            generator = self._parseJoinGenerator(element)
+        return generator
+
+    def _parseRandomGenerator(self, element):
+        values = []
+        probabilities = []
+        for child in element:
+            if child.tag == "item":
+                i = self._parseItem(child)
+                values.append(i['value'])
+                if 'probability' in i:
+                    probabilities.append(float(i['probability']))
+                else:
+                    probabilities.append(1)
+        #Probabilities are normalized
+        probabilities = [float(i)/sum(probabilities) for i in probabilities]
+        return RandomGenerator(values, probabilities)
+
+    def _parseSpaceJoinGenerator(self, element):
+        items = self._parseItems(element)
+        values = list(map(lambda item: item['value'], items))
+        return JoinGenerator(values, " ")
+
+    def _parseJoinGenerator(self, element):
+        separator = element.get("separator") if "separator" in element.attrib else ""
+        items = self._parseItems(element)
+        values = list(map(lambda item: item['value'], items))
+        return JoinGenerator(values, separator)
+
+    def _parseItems(self, children):
+        items = []
+        for child in children:
+            if child.tag == "item":
+                items.append(self._parseItem(child))
+        return items
+
+    def _parseItem(self, element):
+        d = {}
+        d.update(element.attrib)
+        if self._hasChildren(element):
+            #Use only 1st element
+            v = self._parseGenerator(element[0])
+        else:
+            v = element.text
+        d['value'] = v
+        return d
+
+    def _hasChildren(self, element):
+        return bool(len(element))
--- django/ranshigen/models.py
+++ django/ranshigen/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from generators import *
 
 class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, default=None)
@@ -12,3 +13,19 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+class Generator(models.Model):
+    id = models.CharField(primary_key=True, max_length=252, unique=True)
+    data = models.TextField()
+
+    def __str__(self):
+        return self.id
+
+    def generate(self, count=1):
+        parser = XmlParser()
+        generator = parser.fromString(str(self.data))
+        return generator.generate(count)
+
+    class Meta:
+        verbose_name = 'Generator'
+        verbose_name_plural = 'Generators'
